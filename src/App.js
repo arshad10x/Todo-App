f777beb26212b8be9ee5c6a575e776af5926996d
Arshad Ahmed <arshadahmed10x@gmail.com>
2023-04-09 02:34:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function App() {
+  const [todos, setTodos] = useState([
+    "Learn React.js",
+    "Learn JS",
+    "Learn css"
+  ]);
+
+  const [inputTodo, setInputTodo] = useState("");
+  const handleAddTask = () => {
+    setTodos([...todos, inputTodo]);
+    setInputTodo("");
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => setInputTodo(e.target.value)}
+        value={inputTodo}
+      />
+      <button onClick={handleAddTask}>ADD</button>
+      <div>
+        {todos.map((todos, index) => (
+          <p key={index}>{todos}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
