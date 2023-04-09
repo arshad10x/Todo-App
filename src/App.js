f13b8138e504f42ef173a607e0cff58498c667be
Arshad Ahmed <arshadahmed10x@gmail.com>
2023-04-09 10:44:55 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@ import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [todos, setTodos] = useState([
-    "Learn React.js",
-    "Learn JS",
-    "Learn css",
-    "Learn Sass",
-    "Learn ",
-  ]);
-
+  const [todos, setTodos] = useState([]);
   const [inputTodo, setInputTodo] = useState("");
+
   const handleAddTask = () => {
-    setTodos([...todos, inputTodo]);
+    setTodos([
+      ...todos,
+      {
+        task: inputTodo,
+        id: Date.now().toString(),
+        isCompleted: false,
+      },
+    ]);
     setInputTodo("");
   };
 
   // Deleting single Task
-  const handleDeleteTask = (deletedTodo) => {
-    const restTodos = todos.filter((todo) => todo !== deletedTodo);
+  const handleDeleteTask = (deletedTodoId) => {
+    const restTodos = todos.filter((todo) => todo.id !== deletedTodoId);
     setTodos(restTodos);
   };
+
+  // Filter completed and incompleted task
+
+  const toggleCompleteTask = (taskId) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === taskId) {
+        return {
+          ...todo,
+          isCompleted: !todo.isCompleted,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+    console.log(JSON.stringify(newTodos));
+  };
+
   return (
     <div>
       <input
@@ -32,17 +50,26 @@ export default function App() {
         ADD
       </button>
       <div>
-        {todos.map((todo, index) => (
-          <div className="todo" key={index}>
-            <span>{todo}</span>
-            <button
-              className="btn-delete"
-              onClick={() => handleDeleteTask(todo)}
-            >
-              Delete
-            </button>
-          </div>
-        ))}
+        {todos.length > 0 ? (
+          todos.map((todo, index) => (
+            <div className="todo" key={index}>
+              <input
+                type="checkbox"
+                value={todo.isCompleted}
+                onChange={() => toggleCompleteTask(todo.id)}
+              />
+              <span>{todo.task}</span>
+              <button
+                className="btn-delete"
+                onClick={() => handleDeleteTask(todo.id)}
+              >
+                Delete
+              </button>
+            </div>
+          ))
+        ) : (
+          <p>No Task avaliable</p>
+        )}
       </div>
     </div>
   );
